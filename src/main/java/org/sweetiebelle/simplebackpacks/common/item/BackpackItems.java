2751391068c16ebbb5ab2e25f9d9d75751eedadc
Marty E. Plummer <hanetzer@startmail.com>
2020-07-31 00:37:26 -0500

    backpacks: small cleanup
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- src/main/java/org/sweetiebelle/simplebackpacks/common/item/BackpackItems.java
+++ src/main/java/org/sweetiebelle/simplebackpacks/common/item/BackpackItems.java
@@ -12,12 +12,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class BackpackItems {
+    private static final Item.Properties BACKPACK_PROPS = new Item.Properties().defaultMaxDamage(0).maxStackSize(1).group(ItemGroup.TOOLS);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SimpleBackpacks.MODID);;
 
-    public static final RegistryObject<ItemBackpack> LEATHER_BACKPACK = register("leather_backpack", () -> new ItemBackpack(new Item.Properties().defaultMaxDamage(0).maxStackSize(1).group(ItemGroup.TOOLS), BackpackType.LEATHER));
-    public static final RegistryObject<ItemBackpack> IRON_BACKPACK = register("iron_backpack", () -> new ItemBackpack(new Item.Properties().defaultMaxDamage(0).maxStackSize(1).group(ItemGroup.TOOLS), BackpackType.IRON));
-    public static final RegistryObject<ItemBackpack> GOLD_BACKPACK = register("gold_backpack", () -> new ItemBackpack(new Item.Properties().defaultMaxDamage(0).maxStackSize(1).group(ItemGroup.TOOLS), BackpackType.GOLD));
-    public static final RegistryObject<ItemBackpack> DIAMOND_BACKPACK = register("diamond_backpack", () -> new ItemBackpack(new Item.Properties().defaultMaxDamage(0).maxStackSize(1).group(ItemGroup.TOOLS), BackpackType.DIAMOND));
+
+    public static final RegistryObject<ItemBackpack> LEATHER_BACKPACK = register("leather_backpack", () -> new ItemBackpack(BACKPACK_PROPS, BackpackType.LEATHER));
+    public static final RegistryObject<ItemBackpack> IRON_BACKPACK = register("iron_backpack", () -> new ItemBackpack(BACKPACK_PROPS, BackpackType.IRON));
+    public static final RegistryObject<ItemBackpack> GOLD_BACKPACK = register("gold_backpack", () -> new ItemBackpack(BACKPACK_PROPS, BackpackType.GOLD));
+    public static final RegistryObject<ItemBackpack> DIAMOND_BACKPACK = register("diamond_backpack", () -> new ItemBackpack(BACKPACK_PROPS, BackpackType.DIAMOND));
 
     private static final <T extends ItemBackpack> RegistryObject<T> register(String name, Supplier<T> sup) {
         return ITEMS.register(name, sup);
